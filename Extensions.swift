b77a20d295837f72e74a7f2d1648e6f2b3003243
Simon Bestler <68077359+bestler@users.noreply.github.com>
2023-04-14 12:58:25 +0200

    combined all the extension in own file

---
+++ Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  PosterExtendAR
+//
+//  Created by Simon Bestler on 14.04.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIScreen {
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+        @unknown default:
+                self = .up
+        }
+    }
+}
